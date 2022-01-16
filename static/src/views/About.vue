525e97c9a383b1ea82aea14a0e054ba04cc27072
nizzy94 <shark.safety23@gmail.com>
2022-01-16 00:27:30 +0000

    misions vission added

--- static/src/views/About.vue
+++ static/src/views/About.vue
@@ -5,12 +5,17 @@
         <section>
           <about-blog />
         </section> 
+        <section class="q-py-xl" style="padding-bottom: 200px; padding-top:200px">
+          <mission-vision />
+        </section>
+
     </template>
   </Base>
 </template>
 
 <script setup>
 import AboutBlog from "../components/About/AboutBlog.vue";
+import MissionVision from "../components/About/MissionVision.vue";
 import Base from "../components/layouts/Base.vue"
 </script>
 
